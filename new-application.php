7f613b79f077046fb7cb9f146db4a535f6bf4bdd
The-Demp <sean@demps.changeip.net>
2018-05-24 22:01:01 -0700

    Added Personal Info page

--- new-application.php
+++ new-application.php
@@ -2,28 +2,30 @@
 <?php include 'layout-helpers.php'; ?>
 <html>
 <head>
-	<title>New application</title>
+	<title>New Application</title>
 </head>
 <body>
-	<form action="test/testsubmit.php" method="post">
+	<h1>New Application</h1>
+	<p style="color:red">A * next to a field indicates it is required.</p>
+	<form action="personal-info.php" method="post">
 		<p>
-			What type of student are you?<br/>
+			What type of student are you (*)?<br/>
 			<?php renderSelectOne($conn, "studentType", "STUDENT_TYPE"); ?>
 		</p>
 		<p>
-			<label for="college">Which college are you applying to?</label>
+			<label for="college">Which college are you applying to (*)?</label>
 			<?php renderDropdown($conn, "college", "COLLEGE"); ?>
 		</p>
 		<p>
-			What type of degree are you applying for?<br/>
+			What type of degree are you applying for (*)?<br/>
 			<?php renderSelectOne($conn, "degreeType", "DEGREE_TYPE"); ?>
 		</p>
 		<p>
-			<label for="major">Which major are you applying to?</label>
+			<label for="major">Which major are you applying to (*)?</label>
 			<?php renderDropdown($conn, "major", "DEGREE"); ?>
 		</p>
 		<p>
-			<label for="term">Which term are you applying for?</label>
+			<label for="term">Which term are you applying for (*)?</label>
 			<?php renderDropdown($conn, "term", "TERM"); ?>
 		</p>
 		<button type="button" onclick="window.location='new-application.php';">Cancel</button>
---
+++ personal-info.php
@@ -0,0 +1,80 @@
+<?php include 'sql-begin.php'; ?>
+<?php include 'layout-helpers.php'; ?>
+<html>
+<head>
+	<title>Personal Information</title>
+</head>
+<body>
+	<h1>Personal Information</h1>
+	<p style="color:red">A * next to a field indicates it is required.</p>
+	<form action="test/testsubmit.php" method="post">
+		<p>
+			First Name (*):<br/>
+			<input type="text" name="fName"/>
+		</p>
+		<p>
+			Last Name (*):<br/>
+			<input type="text" name="lName"/>
+		</p>
+		<p>
+			Preferred Name:<br/>
+			<input type="text" name="prefName"/>
+		</p>
+		<p>
+			Date of Birth (DD/MM/YYYY) (*):<br/>
+			<input type="text" name="dob"/>
+		</p>
+		<p>
+			Street Address (*):<br/>
+			<input type="text" name="addr"/>
+		</p>
+		<p>
+			City (*):<br/>
+			<input type="text" name="city"/>
+		</p>
+		<p>
+			<label for="state">State (*):</label>
+			<?php renderDropdown($conn, "state", "STATE", "STATE_CODE"); ?>
+		</p>
+		<p>
+			ZIP code (*):<br/>
+			<input type="text" name="zip"/>
+		</p>
+		<p>
+			Phone Number (*):<br/>
+			<input type="text" name="phone"/>
+		</p>
+		<p>
+			Are you a US Citizen (*)?<br/>
+			<?php renderYesNo("usCitizen"); ?>
+		</p>
+		<p>
+			Is English your native language (*)?<br/>
+			<?php renderYesNo("enNative"); ?>
+		</p>
+		<p>
+			What is your gender (*)?<br/>
+			<?php renderSelectOne($conn, "gender", "GENDER"); ?>
+		</p>
+		<p>
+			What is your veteran status (*)?<br/>
+			<?php renderSelectOne($conn, "vetStatus", "VET_STATUS"); ?>
+		</p>
+		<p>
+			If applicable, which military branch(es) have you served for (select all that apply)?<br/>
+			<?php renderSelectMany($conn, "militaryBranch", "MILITARY_BRANCH"); ?>
+		</p>
+		<p>
+			Are you Hispanic/Latino origin (*)?<br/>
+			<?php renderYesNo("hispLat"); ?>
+		</p>
+		<p>
+			Which race/ethnicity do you identify as (select all that apply)?<br/>
+			<?php renderSelectMany($conn, "race", "RACE"); ?>
+		</p>
+		<button type="button" onclick="window.location='new-application.php';">Cancel</button>
+		<input type="submit" value="Next ->"/>
+	</form>
+</body>
+</html>
+<?php include 'sql-end.php'; ?>
