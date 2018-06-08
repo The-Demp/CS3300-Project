4256d27eb4e5f51d4bdc028543c1bb94de695318
The-Demp <sean@demps.changeip.net>
2018-06-07 20:55:16 -0700

    Did optimization on copytoampps.bat

--- copytoampps.bat
+++ copytoampps.bat
@@ -2,5 +2,7 @@
 SET amppsPath="%ProgramFiles(x86)%\Ampps\www\CS3300-Project"
 SET sourcePath="%~dp0"
 
-xcopy %sourcePath:~0,-2%" %amppsPath% /S /Y /I
-for /f %%F in ('dir %amppsPath% /b /a-d ^| findstr /vile ".html .php .js .css"') do del %amppsPath:~0,-1%\%%F"
+for %%e in (html php js css) do (
+	xcopy %sourcePath:~0,-2%\*.%%e" %amppsPath% /S /Y /I
+)
+REM for /f %%F in ('dir %amppsPath% /b /a-d ^| findstr /vile ".html .php .js .css"') do del %amppsPath:~0,-1%\%%F"
